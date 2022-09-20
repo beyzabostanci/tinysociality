47d9e094e774b01936adaae90278d694bfd7dc2a
beyzabostanci <beyza_bostanc@hotmail.com>
2022-09-20 17:59:39 +0300

    needs file structure still, not done

---
+++ pages/_app.jsx
@@ -0,0 +1,22 @@
+import { AuthContextProvider } from "context/AuthContext";
+import Head from "next/head";
+
+
+import "@/styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <title>Students Store</title>
+                <link rel='icon' href='/favicon.png' />
+            </Head>
+
+            <AuthContextProvider>
+                <Component {...pageProps} />
+            </AuthContextProvider>
+        </>
+    );
+}
+
+export default appWithTranslation(MyApp);
