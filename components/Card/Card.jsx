16116aff0ac37aa60c94b28d8043a85365ac119d
beyzabostanci <beyza_bostanc@hotmail.com>
2022-10-07 11:13:48 +0300

    dealing with opacity function

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -4,11 +4,11 @@ import * as React from "react";
 export default function Card() {
 return (
 <>
-<div className="w-[492px] mr-[42px] rounded shadow-lg flex flex-row h-[565px] border border-green-500">
-<div className="w-32 bg-pink-500 border items-center content-center justify-center border-pink-500 flex">
-<Image width={100} height={100} src="/images/xxx.jpg" alt=""  />
+<div className="w-[492px] mr-[42px] rounded shadow-lg flex flex-row h-[565px]">
+<div className="w-32 bg-pink-500 border items-center justify-center flex">
+<Image width={30} height={30} src="/images/xxx.jpg" alt=""  />
 </div>
-<div className="px-5 py-4 border  border-yellow-600 flex flex-col">
+<div className="px-5 py-4 border flex flex-col">
     <div className="flex justify-between">
     <div className="text-sm mb-2 border text-gray-400">21/09/2022 18:18</div>
     <div className="flex border w-24 justify-around">
--- components/Company/Company.jsx
+++ components/Company/Company.jsx
@@ -1,27 +1,21 @@
+import { set } from "date-fns/esm";
 import Image from "next/image";
 import { useState } from "react";
 
-export default function Company (){
-    const [opacity, setOpacity] = useState(0)
-    const buttonHandler = () => {
-        const elem = document.querySelectorAll(".faded");
-        if (elem.style.opacity === "0.7"){
-            elem.style.opacity = "1";
-        }else{
-            elem.style.opacity ="0.7";
-        }
-
-
-}
+export default function Company ({button}){
+   const [isActive, setIsActive] = useState(false);
+ const buttonHandler = () => {
+    setIsActive(current => !current)
+ }
     return(
         <>
-<div className=" w-[88px] border bg-sidebar h-screen flex flex-col">
-    <button onClick={() => setOpacity(1)} className="w-[50px] h-[50px] border overflow-hidden rounded-lg  mt-7 mb-4"><Image src="/images/company1.png" width={"50px"} height={"50px"}  alt="company1" /></button>
-    <button className="w-[50px] h-[50px] border overflow-hidden rounded-lg faded mt-4 mb-4"><Image src="/images/company99.png" width={"50px"} height={"50px"} alt="company1" /></button>
-    <button className="w-[50px] h-[50px] border overflow-hidden rounded-lg faded mt-4 mb-4"><Image src="/images/company3.png" width={"50px"} height={"50px"}  alt="company1" /></button>
-    <button className="w-[50px] h-[50px] border overflow-hidden rounded-lg faded mt-4 mb-4"><Image src="/images/company4.png" width={"50px"} height={"50px"}  alt="company1" /></button>
-    <button className="w-[50px] h-[50px] border overflow-hidden rounded-lg  faded mt-4 mb-4"><Image src="/images/company5.png" width={"50px"} height={"50px"}  alt="company1" /></button>
-    <button className="w-[50px] h-[50px] border overflow-hidden rounded-lg faded mt-4 mb-4"><Image src="/images/company6.png" width={"50px"} height={"50px"}  alt="company1" /></button>
+<div className=" w-[88px] bg-sidebar h-screen px-[18px] flex flex-col">
+    <button onClick={buttonHandler} className="w-[50px] h-[50px] border overflow-hidden rounded-lg   mt-7 mb-4"><Image src="/images/company1.png" width={"50px"} height={"50px"}  alt="company1" /></button>
+    <button onClick={buttonHandler} className="w-[50px] h-[50px] border overflow-hidden rounded-lg  mt-4 mb-4"><Image src="/images/company99.png" width={"50px"} height={"50px"} alt="company1" /></button>
+    <button onClick={buttonHandler} className="w-[50px] h-[50px] border overflow-hidden rounded-lg  mt-4 mb-4"><Image src="/images/company3.png" width={"50px"} height={"50px"}  alt="company1" /></button>
+    <button onClick={buttonHandler} className="w-[50px] h-[50px] border overflow-hidden rounded-lg  mt-4 mb-4"><Image src="/images/company4.png" width={"50px"} height={"50px"}  alt="company1" /></button>
+    <button onClick={buttonHandler} className="w-[50px] h-[50px] border overflow-hidden rounded-lg  mt-4 mb-4"><Image src="/images/company5.png" width={"50px"} height={"50px"}  alt="company1" /></button>
+    <button onClick={buttonHandler} className="w-[50px] h-[50px] border overflow-hidden rounded-lg  mt-4 mb-4"><Image src="/images/company6.png" width={"50px"} height={"50px"}  alt="company1" /></button>
 </div>
         </>
     )
